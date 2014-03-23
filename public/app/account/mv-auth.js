3960544c3fb538f4b8c81427228904bf6a6074a6
Megan Sharon <me@megansharon.com>
2014-03-23 08:59:55 -0400

    refactor auth server code

--- public/app/account/mv-auth.js
+++ public/app/account/mv-auth.js
@@ -1,10 +1,11 @@
 angular.module('app')
-  .factory('mvAuth', function(Restangular, mvIdentity) {
+  .factory('mvAuth', function(Restangular, mvIdentity, $q) {
     var service = Restangular.all('login');
 
     return {
       authenticateUser: function(username, password) {
-        var promise = service.post({ username: username, password: password }).then(
+        var promise = service.post({ username: username, password: password })
+          .then(
             function (res) {
               if (res.success) {
                 mvIdentity.currentUser = res.user;
@@ -14,9 +15,9 @@ angular.module('app')
               }
             },
             function (err) {
-              return err;
+              return $q.reject(err);
             }
-        );
+          );
         return promise;
       }
     }
