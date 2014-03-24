9471ddc05249637b27cc928d72d74450151e07be
Megan Sharon <me@megansharon.com>
2014-03-24 04:36:14 -0400

    show list of users

--- public/app/admin/mv-admin-ctrl.js
+++ public/app/admin/mv-admin-ctrl.js
@@ -1,4 +1,4 @@
 angular.module('app')
   .controller('mvAdminCtrl', function ($scope, mvUserManager) {
-
+    $scope.userManager = mvUserManager;
   });
--- public/app/service/mv-user-manager.js
+++ public/app/service/mv-user-manager.js
@@ -1,4 +1,8 @@
 angular.module('mv.managers.User', ['mv.resource.admin'])
   .factory('mvUserManager', function (mvUserAdminResource) {
+    var _users = mvUserAdminResource.getUsers().getList();
 
+    return {
+      users: _users
+    };
   });
